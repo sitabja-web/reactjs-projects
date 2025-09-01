f162cd0365c934005f679806de2378f69e046f45
sitabja biswas <sitabjabiswas633@gmail.com>
2025-09-01 23:23:33 +0530

    Add files via upload

---
+++ intro.jsx
@@ -0,0 +1,30 @@
+// react is javascript library ui interface barane ke liye 
+
+// react is made of small parts  
+
+// components => react is made up of small parts me jisse components kehte  hai   example => nav bar ,  button , footer these are all alag comppnent 
+
+// javascript +  xml react me html jaisa code likhen ka tarikha  
+
+
+function Hello() {
+  return <h1>Hello World!</h1>;
+}
+
+
+// Props (Properties)
+
+// Data ko ek component se doosre component tak bhejne ka tarika.
+
+// Example: Card title="My Blog"
+
+// State
+
+// Component ke andar ka data jo change hota hai.
+
+// Example: Counter app me number badhta hai → state change hoti hai.
+
+// react use virtual dom  a new dom is create in the memorhy beacause of these because of these react uses virtual dom because of which it is fater beacues anything is changed   it is updated this makes the app faster 
+
+// use these to install react 
+// npx create-react-app my-react-app   
